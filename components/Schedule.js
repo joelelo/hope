7f6de2e8b9e2103ec7b871341f60573d18a76843
joelelo <elioenaijoel@gmail.com>
2022-07-14 15:40:24 +0700

    Schedule+edit

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,9 +1,172 @@
-import React from 'react';
+import React from "react";
 
 const Schedule = () => {
-	return <div id="schedule" className='grid-cols-8 grid-rows-15'>
-        <div></div>
-    </div>;
+  const schedule = [
+    [
+      { content: "06:00", span: "1" },
+      { content: "prayer", span: "7" },
+    ],
+    [
+      { content: "07:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "08:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "09:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "10:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "11:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "12:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "14:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "16:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "18:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "20:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "22:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+    [
+      { content: "24:00", span: "1" },
+      { content: "2", span: "1" },
+      { content: "3", span: "1" },
+      { content: "4", span: "1" },
+      { content: "5", span: "1" },
+      { content: "6", span: "1" },
+      { content: "7", span: "1" },
+      { content: "8", span: "1" },
+    ],
+  ];
+  return (
+    <div
+      id="schedule"
+      className="grid w-full grid-cols-8 pb-20 text-center text-vin4"
+    >
+      <div className="bg-vin2">Week</div>
+      <div className="bg-vin2">Mon</div>
+      <div className="bg-vin2">Tue</div>
+      <div className="bg-vin2">Wed</div>
+      <div className="bg-vin2">Thu</div>
+      <div className="bg-vin2">Fri</div>
+      <div className="bg-vin2">Sat</div>
+      <div className="bg-vin2">Sun</div>
+      {schedule.map((row) => {
+        return row.map((block) => {
+          return (
+            <div
+              className={
+                block.span === "1"
+                  ? "col-span-1 bg-vin2"
+                  : block.span === "2"
+                  ? "col-span-2 bg-vin2"
+                  : block.span === "3"
+                  ? "col-span-3 bg-vin2"
+                  : block.span === "4"
+                  ? "col-span-4 bg-vin2"
+                  : block.span === "5"
+                  ? "col-span-5 bg-vin2"
+                  : block.span === "6"
+                  ? "col-span-6 bg-vin2"
+                  : "col-span-7 bg-vin2"
+              }
+            >
+              {block.content}
+            </div>
+          );
+        });
+      })}
+    </div>
+  );
 };
 
 export default Schedule;
--- pages/index.js
+++ pages/index.js
@@ -1,106 +1,129 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from '../components/Navbar';
-import Content from '../components/Content';
-import { AiOutlinePlayCircle, AiOutlinePauseCircle } from 'react-icons/ai';
-import { useState, useRef, useEffect } from 'react';
-import { Wave } from '@foobar404/wave';
-import Schedule from '../components/Schedule';
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import Content from "../components/Content";
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
+import { useState, useRef, useEffect } from "react";
+import { Wave } from "@foobar404/wave";
+import Schedule from "../components/Schedule";
+import EditSchedule from "../components/EditSchedule";
 export default function Home() {
-	const audioRef = useRef();
-	const canvasRef = useRef();
-	const [playing, setPlaying] = useState(true);
-	let wave;
-	useEffect(() => {
-		if (wave === undefined) {
-			try {
-				console.log(typeof(wave))
-				wave = new Wave(audioRef.current, canvasRef.current);
-				wave.addAnimation(
-					new wave.animations.Lines({
-						lineColor: 'red',
-						count: 100,
-					})
-				);
-				console.log(typeof(wave))
-			} catch (error) {
-				console.log('Refresh page!!');
-			}
-		}
-	}, []);
-	const handlePlay = () => {
-		const prev = playing;
-		setPlaying(!prev);
-		prev ? audioRef.current.play() : audioRef.current.pause();
-	};
-	return (
-		<div className="relative h-screen w-screen bg-vin4 overflow-x-hidden lg:overflow-x-visible">
-			<audio ref={audioRef} src="/audio/sample2.mp3" />
-			<Navbar />
-			<div className="relative overflow-hidden lg:w-full w-2s aspect-img">
-				<div className="absolute text-titm lg:text-tit z-20 top-1/4 lg:top-1/3 left-1/20  font-bold text-vin2 italic">Hope Radio</div>
-				<div className="absolute text-titm lg:text-tit z-20 top-1/3 lg:top-2/5 left-1/10 text-red-500 font-cursive">Atmosfir</div>
-				{playing ? (
-					<AiOutlinePlayCircle onClick={handlePlay} className="absolute z-10 text-vin2 w-1/3 h-1/3 bottom-1/5 left-1/10 lg:w-1/5 lg:h-1/5 lg:left-1/5" />
-				) : (
-					<AiOutlinePauseCircle onClick={handlePlay} className="absolute z-10 text-vin2 w-1/3 h-1/3 bottom-1/5 left-1/10 lg:w-1/5 lg:h-1/5 lg:left-1/5" />
-				)}
-				<canvas ref={canvasRef} className="absolute z-20 right-1/5 top-3/5 w-1/5 h-1/10 "></canvas>
-				<Image src={'/radio14.jpg'} layout="fill" priority="true" />
-			</div>
-			<div className="w-1/2">{/* <Image src={'/radio4clean.png'} height={521} width={864} layout="responsive" /> */}</div>
-			{/* <Player /> */}
-			<Content
-				head={'Engkau Baik'}
-				con={
-					<>
-						<p className="text-vin1 basis-1/2 pb-10">
-							<span className="text-vin2 font-bold">ENGKAU BAIK </span>
-							Sebuah persembahan Pujian baru dari Bekasi Hope Church & Jonah Chen, tentang kebaikkan Tuhan, kiranya lagu ini dapat
-							menjadi menjadi berkat bagi Gereja-Gereja karena mudah di nyanyikan untuk jemaat semua. ENGKAU BAIK (Jonah Chen) Pujian
-							hormat bagi Allah tak terukur kebaikkannya setiap hari ku menikmati setiap waktu Engkau baik Reff: Engkau baik sangat baik
-							besar setiaMu Tuhan di hidupku Engkau baik sangat baik besar setiaMu TUhan di hidupku ku nyanyi haleluyah haleluyah,
-							haleluluyah Besar Tuhan di hidupku (2x)
-						</p>
-						<iframe
-							className="basis-1/3 aspect-video m-auto drop-shadow-xl"
-							width=""
-							height=""
-							src="https://www.youtube.com/embed/0vZRFq09zeE"
-							title="YouTube video player"
-							frameborder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							allowFullScreen
-						></iframe>
-					</>
-				}
-			/>
-			<Content
-				head={'Tetap Percaya'}
-				con={
-					<>
-						<iframe
-							className="basis-1/3 aspect-video m-auto drop-shadow-xl"
-							width=""
-							height=""
-							src="https://www.youtube.com/embed/-7zg8wusTe0"
-							title="YouTube video player"
-							frameborder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							allowfullscreen
-						></iframe>
-						<p className="text-vin1 basis-1/2 pb-10">
-							<span className="text-vin2 font-bold">TETAP PERCAYA by: Jonah Chen</span> Jonah Chen sangat besyukur mendapatkan
-							kesempatan untuk bisa menyanyikan lagu <q>TETAP PERCAYA.</q> di studio Unlimited Worship serta pujian ini bisa menjadi
-							berkat buat pemirsa di rumah yang menyaksikan chanel 1st media chanel 89 Praise, lagu ini release tahun 2021 yang lalu dan
-							sudah bisa di dengerin di semua platform digital. kiranya Pujian ini menjadi berkat buat saudara semua untuk tetap percaya
-							kepada Tuhan yg kuasanya dan janjiNya tidak berubah dulu sekarang dan selamanya.
-						</p>
-					</>
-				}
-			/>
-			<Content head={'Schedule'} con={<Schedule />} />
-		</div>
-	);
+  const audioRef = useRef();
+  const canvasRef = useRef();
+  const [playing, setPlaying] = useState(true);
+  let wave;
+  useEffect(() => {
+    if (wave === undefined) {
+      try {
+        console.log(typeof wave);
+        wave = new Wave(audioRef.current, canvasRef.current);
+        wave.addAnimation(
+          new wave.animations.Lines({
+            lineColor: "red",
+            count: 100,
+          })
+        );
+        console.log(typeof wave);
+      } catch (error) {
+        console.log("Refresh page!!");
+      }
+    }
+  }, []);
+  const handlePlay = () => {
+    const prev = playing;
+    setPlaying(!prev);
+    prev ? audioRef.current.play() : audioRef.current.pause();
+  };
+  return (
+    <div className="relative h-screen w-screen overflow-x-hidden bg-vin4 lg:overflow-x-visible">
+      <audio ref={audioRef} src="/audio/sample2.mp3" />
+      <Navbar />
+      <div className="relative aspect-img w-2s overflow-hidden lg:w-full">
+        <div className="absolute top-1/4 left-1/20 z-20 text-titm font-bold italic  text-vin2 lg:top-1/3 lg:text-tit">
+          Hope Radio
+        </div>
+        <div className="absolute top-1/3 left-1/10 z-20 font-cursive text-titm text-red-500 lg:top-2/5 lg:text-tit">
+          Atmosfir
+        </div>
+        {playing ? (
+          <AiOutlinePlayCircle
+            onClick={handlePlay}
+            className="absolute bottom-1/5 left-1/10 z-10 h-1/3 w-1/3 text-vin2 lg:left-1/5 lg:h-1/5 lg:w-1/5"
+          />
+        ) : (
+          <AiOutlinePauseCircle
+            onClick={handlePlay}
+            className="absolute bottom-1/5 left-1/10 z-10 h-1/3 w-1/3 text-vin2 lg:left-1/5 lg:h-1/5 lg:w-1/5"
+          />
+        )}
+        <canvas
+          ref={canvasRef}
+          className="absolute right-1/5 top-3/5 z-20 h-1/10 w-1/5 "
+        ></canvas>
+        <Image src={"/radio14.jpg"} layout="fill" priority="true" />
+      </div>
+      <Content
+        head={"Engkau Baik"}
+        con={
+          <>
+            <p className="basis-1/2 pb-10 text-vin1">
+              <span className="font-bold text-vin2">ENGKAU BAIK </span>
+              Sebuah persembahan Pujian baru dari Bekasi Hope Church & Jonah
+              Chen, tentang kebaikkan Tuhan, kiranya lagu ini dapat menjadi
+              menjadi berkat bagi Gereja-Gereja karena mudah di nyanyikan untuk
+              jemaat semua. ENGKAU BAIK (Jonah Chen) Pujian hormat bagi Allah
+              tak terukur kebaikkannya setiap hari ku menikmati setiap waktu
+              Engkau baik Reff: Engkau baik sangat baik besar setiaMu Tuhan di
+              hidupku Engkau baik sangat baik besar setiaMu TUhan di hidupku ku
+              nyanyi haleluyah haleluyah, haleluluyah Besar Tuhan di hidupku
+              (2x)
+            </p>
+            {/* <iframe
+              className="m-auto aspect-video basis-1/3 drop-shadow-xl"
+              width=""
+              height=""
+              src="https://www.youtube.com/embed/0vZRFq09zeE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe> */}
+          </>
+        }
+      />
+      <Content
+        head={"Tetap Percaya"}
+        con={
+          <>
+            {/* <iframe
+              className="m-auto aspect-video basis-1/3 drop-shadow-xl"
+              width=""
+              height=""
+              src="https://www.youtube.com/embed/-7zg8wusTe0"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe> */}
+            <p className="basis-1/2 pb-10 text-vin1">
+              <span className="font-bold text-vin2">
+                TETAP PERCAYA by: Jonah Chen
+              </span>{" "}
+              Jonah Chen sangat besyukur mendapatkan kesempatan untuk bisa
+              menyanyikan lagu <q>TETAP PERCAYA.</q> di studio Unlimited Worship
+              serta pujian ini bisa menjadi berkat buat pemirsa di rumah yang
+              menyaksikan chanel 1st media chanel 89 Praise, lagu ini release
+              tahun 2021 yang lalu dan sudah bisa di dengerin di semua platform
+              digital. kiranya Pujian ini menjadi berkat buat saudara semua
+              untuk tetap percaya kepada Tuhan yg kuasanya dan janjiNya tidak
+              berubah dulu sekarang dan selamanya.
+            </p>
+          </>
+        }
+      />
+      <Content head={"Schedule"} con={<Schedule />} />
+      <Content head={"Edit Schedule"} con={<EditSchedule />} />
+    </div>
+  );
 }
 //absolute top-0 right-0 h-auto w-1/2
